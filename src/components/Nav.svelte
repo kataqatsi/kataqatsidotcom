76d395d838ad8c1279c496d43dbcbc588dda4452
David Anderson <kataqatsi@gmail.com>
2024-12-24 18:02:54 -0700

    fix

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -29,7 +29,7 @@
 	bg-gray-50 bg-opacity-60 py-8 text-gray-900 dark:border-gray-700 dark:bg-gray-900
 	dark:text-gray-100 sm:pb-16"
 >
-	<a href="#skip" class="skip-nav">Skip to content</a>
+	<!-- <a href="#skip" class="skip-nav">Skip to content</a> -->
 	<MobileMenu />
 	<ul class="ml-[-0.60rem] flex">
 		<li>
